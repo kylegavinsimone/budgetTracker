a67cc5801cd5e2f927667a64be2383a307b4b510
kylegavinsimone <kylesimone@outlook.com>
2021-04-19 14:01:37 -0700

    finished with study group

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = process.env.PORT || 8888;
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetTrackerdb",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
+// routes
 app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
